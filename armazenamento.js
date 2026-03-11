d23408e47d4db03e7e0d292948e4da9d0081b4e9
EDUARDO TREVISAN BRUSCO <eduardo.brusco@portalsesisp.org.br>
2026-03-11 10:06:09 -0300

    Commit inicial: estrutura do projeto

---
+++ armazenamento.js
@@ -0,0 +1,5 @@
+let nome = "joao"; 
+let idade = 25;
+
+console.log("nome:"+nome);
+console.log("idade:" + idade)
---
+++ escopo.js
@@ -0,0 +1,10 @@
+function exemplo (){
+    let numero = 10;
+
+    if( numero >5){
+        let mensagem = "O numero é maior que 5.";
+        console.log(mensagem); // acessivel dentro do bloco if
+    }
+    console.log(numero);
+}
+exemplo();
