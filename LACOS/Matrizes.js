40e47ade58c64a24299f7adeb34769377c267893
eduardobrusco <eduardobrusco3@gmail.com>
2026-03-31 13:52:13 -0300

    atividades 31/03

---
+++ LACOS/Matrizes.js
@@ -0,0 +1,17 @@
+let alunos = [
+[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9],
+]
+
+for(let i = 0; i < alunos.length; i++){
+    let soma = 0;
+
+    for (let j = 0; j < alunos[i].length; j++){
+        soma += alunos[i][j];
+    }
+
+    let media = soma / alunos[i].length;
+
+    console.log("amédia do aluno: ", i +1, ":", media)
+}
